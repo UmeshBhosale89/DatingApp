8fc7e6f9006b0147a9f3b6cd0b39b3bd4c93aa3f
Bhosale, Umesh <umesh.bhosale@nuance.com>
2022-07-09 09:56:19 +0530

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +15,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -28,15 +34,33 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-services.AddDbContext<DataContext>(options =>{
-  options.UseSqlServer(Config.GetConnectionString("DefaultConnection"));
-});
+
+//move below services to extenstion method
+            services.AddApplicationServices(Config);
+//             services.AddScoped<ITokenService, TokenService>();
+// services.AddDbContext<DataContext>(options =>{
+//   options.UseSqlServer(Config.GetConnectionString("DefaultConnection"));
+// });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
             services.AddCors();
+//move below services to extenstion method
+             services.AddIdentityServices(Config);
+            // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            // .AddJwtBearer(options => 
+            // {
+            //     options.TokenValidationParameters = new TokenValidationParameters
+            //     {
+            //         ValidateIssuerSigningKey=true,
+            //         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Config["TokenKey"])),
+            //         ValidateIssuer=false,
+            //         ValidateAudience=false
+            //     };
+
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +79,7 @@ services.AddDbContext<DataContext>(options =>{
 
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
